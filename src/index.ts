466a725e604abf498a56854f0d4a7abb9b2025e2
Moritz344 <moritzmaier34@gmail.com>
2025-12-19 15:52:52 +0100

    search output for beatmaps and users

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { search, select, Separator } from '@inquirer/prompts';
 import { startServer } from './server.ts';
 import fs from 'fs';
 import path from 'path';
+import chalk from 'chalk';
 
 
 const config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), "utf-8"));
@@ -33,6 +34,7 @@ function main() {
 main();
 
 async function home() {
+  console.clear();
   try {
     const answer = await select({
       message: "Select Option \n",
@@ -73,7 +75,6 @@ async function home() {
 }
 
 async function searchUsername() {
-
   try {
     const answer = await search({
       message: 'Username:',
@@ -101,13 +102,66 @@ async function searchUsername() {
         }));
       },
     });
-    console.log(answer);
+    await showUserData(answer, "osu");
   } catch (err) {
-    await home();
+    if (err instanceof Error && err.name === "ExitPromptError") {
+      await home();
+    }
+  }
+}
+async function showUserData(id: unknown, mode?: string) {
+  console.clear();
+  try {
+    const url = base_url + "users/" + id + "/" + mode;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        "Authorization": `Bearer ${config.access_token}`,
+        "Content-Type": "application/json"
+      },
+    })
+    const data: any = await response.json();
+    const user = {
+      name: data.username,
+      online: data.is_online,
+      join_date: data.join_date,
+      playstyle: data.playstyle,
+      country: data.country.name,
+      follower: data.follower_count,
+      color: data.profile_colour,
+      pp: data.statistics.pp,
+      rank: data.statistics.global_rank,
+      country_rank: data.statistics.country_rank,
+      playtime: data.statistics.play_time
+
+    };
+    console.log(chalk.hex("#FF6FAE")(user.name));
+    let a = [];
+    for (let i = 0; i < user.name.length; i++) {
+      a.push("-");
+    }
+    console.log(a.join(""));
+    if (user.online == true) {
+      console.log(chalk.hex("#7ED321")("Online"));
+    } else {
+      console.log(chalk.hex("#FF4D4D")("Offline"));
+    }
+    console.log("Score: " + user.pp + "pp" + " (Global #" + user.rank + ")" + " (Country #" + user.country_rank + ")");
+    console.log("Country: " + user.country);
+    console.log("Followers: " + user.follower);
+    if (user.playstyle != null) {
+      console.log("Playstyle: " + user.playstyle.join(","));
+    }
+
+    console.log("Join Date: " + new Date(user.join_date).toDateString());
+    await searchUsername();
+  } catch (err) {
+
   }
 
-
 }
+
+
 
 async function searchBeatmap() {
   let currentPage = 1;
@@ -139,21 +193,72 @@ async function searchBeatmap() {
 
         return data.beatmapsets.map((beatmap: any) => ({
           name: beatmap.title,
-          value: beatmap.title,
+          value: beatmap.id,
         }));
       },
     });
+    if (answer) {
+      await showBeatmap(answer);
+    }
   } catch (err) {
-    await home();
+    if (err instanceof Error && err.name === "ExitPromptError") {
+      await home();
+    }
+
   }
 
+
+}
+
+async function showBeatmap(id: unknown) {
+  console.clear();
+  try {
+    const url = base_url + "beatmapsets/" + id;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        "Authorization": `Bearer ${config.access_token}`,
+        "Content-Type": "application/json"
+      },
+    })
+    const data: any = await response.json();
+    const beatmap = {
+      name: data.title,
+      artist: data.artist,
+      creator: data.creator,
+      play_count: data.play_count,
+      status: data.status,
+      video: data.video,
+      bpm: data.bpm,
+    };
+    let a = [];
+    console.log(chalk.hex("#FF4D4D")(beatmap.name));
+    for (let i = 0; i < beatmap.name.length; i++) {
+      a.push("-");
+    }
+    console.log(a.join(""));
+    console.log("Artist: " + beatmap.artist);
+    console.log("Creator: " + beatmap.creator);
+    console.log("Play Count: " + beatmap.play_count);
+    console.log("BPM: " + beatmap.bpm);
+    console.log();
+    console.log("Beatmaps");
+    console.log("--------")
+    for (let i = 0; i < data.beatmaps.length; i++) {
+      console.log(data.beatmaps[i].version, chalk.yellow(data.beatmaps[i].difficulty_rating + "*"));
+    }
+    console.log();
+    await searchBeatmap();
+  } catch (err) {
+
+  }
 
 }
 
 async function searchChangelog() {
   try {
     let choicesList = [];
-    const url = base_url + `changelog?stream=lazer&since=2021`;
+    const url = base_url + `changelog?stream=lazer`;
 
     const response = await fetch(url, {
       method: 'GET',
@@ -179,7 +284,9 @@ async function searchChangelog() {
     });
     await showChangelog(answer);
   } catch (err) {
-    await home();
+    if (err instanceof Error && err.name === "ExitPromptError") {
+      await home();
+    }
   }
 
 }
@@ -187,4 +294,5 @@ async function searchChangelog() {
 async function showChangelog(version: string) {
 
 }
+
 
